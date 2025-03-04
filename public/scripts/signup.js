c91c56eb7b9759139a62db086d633cec7bb4e1f8
Sameer Jadav <sameerjadav010@gmail.com>
2025-03-04 10:13:45 +0530

    feat: client sends signup request

---
+++ public/scripts/signup.js
@@ -0,0 +1,60 @@
+function main() {
+  const form = document.getElementById("signup_form");
+
+  if (!form) {
+    console.error("signup form not found");
+    return;
+  } else if (!(form instanceof HTMLFormElement)) {
+    console.error("element with id 'signup_form' is not a form element");
+    return;
+  }
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("confirm_password");
+
+    if (!password) {
+      console.error("password feild not found");
+      return;
+    } else if (!confirmPassword) {
+      console.error("confirm password feild not found");
+      return;
+    } else if (!email) {
+      console.error("email feild not found");
+      return;
+    }
+
+    if (!(password instanceof HTMLInputElement)) {
+      console.error("element with id 'password' is not an input element");
+      return;
+    } else if (!(confirmPassword instanceof HTMLInputElement)) {
+      console.error(
+        "element with id 'confirm_password' is not an input element",
+      );
+      return;
+    } else if (!(email instanceof HTMLInputElement)) {
+      console.error("element with id 'email' is not an input element");
+      return;
+    }
+
+    if (password.value !== confirmPassword.value) {
+      console.error("password mismatch");
+      return;
+    }
+
+    const data = { email: email.value, password: password.value };
+
+    fetch("/api/auth/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    console.log(data);
+  });
+}
+
+main();
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+
 /**
  * @param {"INFO" | "WARN" | "ERROR"} level
  * @param {string} msg
@@ -8,4 +11,58 @@ export function log(level, msg) {
   const date = now.toLocaleDateString("en-GB");
   const time = now.toLocaleTimeString("en-GB", { hour12: false });
   console.log(`${date} ${time} ${level} ${msg}`);
+}
+
+/**
+ * @typedef {Object} RouteInfo
+ * @property {string} filePath
+ * @property {string} contentType
+ */
+
+/**
+ * @param {string} srcDir
+ * @param {string} baseDir
+ * @returns {Promise<Map<string,RouteInfo>>}
+ */
+export async function getStaticRoutes(srcDir, baseDir) {
+  /** @type {Map<string,RouteInfo>} */
+  let routes = new Map();
+  const entries = await fs.readdir(srcDir);
+  for (const entry of entries) {
+    const fullpath = path.join(srcDir, entry);
+    const stats = await fs.stat(fullpath);
+    if (stats.isDirectory()) {
+      const subRoutes = await getStaticRoutes(fullpath, baseDir);
+      for (const [key, value] of subRoutes) {
+        routes.set(key, value);
+      }
+    } else {
+      let route =
+        "/" + path.relative(baseDir, fullpath).split(path.sep).join("/");
+      const extention = path.extname(entry);
+      switch (extention) {
+        case ".html":
+          route = route.slice(0, -extention.length);
+          if (route.endsWith("index")) {
+            route = route.slice(0, -"index".length);
+            if (route.endsWith("/") && route !== "/") {
+              route = route.slice(0, -1);
+            }
+          }
+          routes.set(route, { filePath: fullpath, contentType: "text/html" });
+          break;
+        case ".css":
+          routes.set(route, { filePath: fullpath, contentType: "text/css" });
+          break;
+        case ".js":
+          routes.set(route, {
+            filePath: fullpath,
+            contentType: "application/javascript",
+          });
+          break;
+        default:
+      }
+    }
+  }
+  return routes;
 }
